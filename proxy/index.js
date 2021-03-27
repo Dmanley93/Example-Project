8c763669fe12659271ceb73642ec736007f78573
Leighton Jenkins <leightonjenkins84@gmail.com>
2021-03-26 23:14:50 -0500

    added node proxy server for apex api

---
+++ proxy/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://public-api.tracker.gg/v2/apex/standard/profile";
+
+app.use(cors());
+
+app.use('', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/json_placeholder`]: '',
+    },
+ }));
+
+ app.listen(PORT, HOST);
