0d6b4b9a6211680c5eb801c0a11f48f96da32187
Leighton Jenkins <leightonjenkins84@gmail.com>
2021-03-27 00:21:05 -0500

    updated index.js to fix access control

--- proxy/index.js
+++ proxy/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
+const { response } = require('express');
 
 const app = express();
 
@@ -8,12 +9,18 @@ const PORT = process.env.PORT || 3000;
 const HOST = "localhost";
 const API_SERVICE_URL = "https://public-api.tracker.gg/v2/apex/standard/profile";
 
+app.use(cors());
+
 app.use('', createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+      }
  }));
 
- app.use(cors()); 
+
+ 
 
  app.listen(PORT);
 
